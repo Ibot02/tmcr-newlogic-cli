d53df5b804c318d6983bf370d32d42bfa41b8372
Ibot02 <jtp.jtp@gmx.de>
2025-02-02 17:01:34 +0100

    Fix? workflow: 3.5
    Co-authored-by: Henny022 <hen.geheim22@gmail.com>

--- docker.nix
+++ docker.nix
@@ -1,11 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (
+  builtins.fetchGit {
+  name = "nixpkgs-unstable-2025-02-02";
+  url = "https://github.com/nixos/nixpkgs/";
+  ref = "refs/heads/nixpkgs-unstable";
+  # Commit hash for nixpkgs-unstable as of 2025-02-02
+  rev = "9189ac18287c599860e878e905da550aa6dec1cd";
+}) {} }:
 let
   bin = pkgs.haskell.lib.justStaticExecutables (pkgs.haskellPackages.callCabal2nix "tmcr-newlogic-cli" ./. {
     tmcr-newlogic-lib = pkgs.haskellPackages.callCabal2nix "tmcr-newlogic-lib" (pkgs.fetchFromGitHub {
       owner = "Ibot02";
       repo = "tmcr-newlogic-lib";
-      rev = "0e00960d9620e90e4e0a9507bc1c00cdbf4d9b33";
-      sha256 = "0gpq0x9vp7fqmbsws1y0lx1zysfsxcv9yx64g9iib7ahxaf6i1qp";
+      rev = "3e2979564780bd2497c01577faf6de96dbc378d3";
+      sha256 = "9kMETUrM2sOKRYQ87Se1ctvfy1hP5mm8m4f7mLPqz00=";
     }) {};
   });
 in pkgs.dockerTools.buildLayeredImage {
