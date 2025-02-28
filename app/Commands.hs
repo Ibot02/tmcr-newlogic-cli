00407f29febb3a21a490cfe80c23e5acba896390
Ibot02 <jtp.jtp@gmx.de>
2025-02-28 21:36:35 +0100

    More Commands
    Co-authored-By: Henny022 <git@henny022.de>

--- app/Commands.hs
+++ app/Commands.hs
@@ -1,32 +1,121 @@
-module Commands (Command(..), commandHelp, commandCompletion, parseCommand) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+module Commands (Command(..), GameContext(..), commandCompletion, parseCommand, LogLevel(..)) where
 
 import System.Console.Haskeline
 import System.Console.Haskeline.Completion
 
 import TMCR.Logic.Merge (GameDef)
+import TMCR.Logic.Common (Thingy)
+import TMCR.Logic.Descriptor (DescriptorName, DescriptorType (Truthy), DescriptorIdent)
+
+import Control.Monad.Reader (MonadReader(..), ReaderT (runReaderT), asks)
+import Control.Monad.Trans (MonadIO(liftIO), lift)
+import Control.Monad
+import Control.Arrow (Arrow(first))
+import TMCR.Logic.Shuffle (RandomSeed)
+import System.Random (mkStdGen)
+import Text.Read (readMaybe)
 
 data Command = CmdExit
-             | CmdHelp [String]
-            --  | CmdIsReachable (DescriptorIdent 'Truthy, [Thingy])
-            --  | CmdStep
-            --  | CmdRun
+             | CmdIsReachable (DescriptorIdent 'Truthy, [Thingy])
+             | CmdStep Integer
+             | CmdRun
+             | CmdRandomSeed
+             | CmdSetSeed RandomSeed
+             | CmdSetLogLevel LogLevel
+
+data GameContext = GameContext
+
+data LogLevel = LogLevelDebug | LogLevelError deriving (Eq, Ord, Show)
 
 commandHelp :: [String] -> String
 commandHelp _ = "todo: help"
 
-parseCommand :: InputT IO Command
+parseCommand :: ReaderT GameContext (InputT IO) Command
 parseCommand = do
-  i <- fmap words <$> getInputLine "> "
-  case i of
-    Nothing -> return CmdExit
-    Just ("exit":_) -> return CmdExit
-    Just ("help":xs) -> return $ CmdHelp xs
-    Just _ -> return $ CmdHelp []
+  i <- fmap words <$> lift (getInputLine "> ")
+  x <- parseCommand' commands CmdExit i
+  maybe parseCommand return x
 
+parseCommand' :: CommandTree a -> a -> Maybe [String] -> ReaderT GameContext (InputT IO) (Maybe a)
+parseCommand' commands onEOF = \case
+    Nothing -> pure $ Just onEOF
+    Just ("help":_) -> printHelp commands >> return Nothing
+    Just xs -> do
+      r <- asks (either (const Nothing) Just . runReaderT (parseCommands'' commands xs))
+      when (null r) $ printHelp commands
+      return r
+
+parseCommands'' :: CommandTree a -> [String] -> ReaderT GameContext (Either [String]) a
+parseCommands'' commands input = do
+  context <- ask
+  lift $ foldr (firstRightWith (<>)) (Left []) $ fmap (`runReaderT` context) $ fmap (`parseCommand''` input) commands
+
+firstRightWith :: (a0 -> a0 -> a0) -> Either a0 a -> Either a0 a -> Either a0 a
+firstRightWith _ (Right a) _ = Right a
+firstRightWith _ _ (Right a) = Right a
+firstRightWith combine (Left x) (Left y) = Left (x `combine` y)
+
+parseCommand'' :: CommandSpec a -> [String] -> ReaderT GameContext (Either [String]) a
+parseCommand'' (CmdSpecHelptext _ x) xs = parseCommand'' x xs
+parseCommand'' (CmdSpecLeaf a) [] = return a
+parseCommand'' (CmdSpecLeaf _) _ = lift $ Left ["unexpected params"]
+parseCommand'' (CmdSpecTarget _) [] = lift $ Left ["Missing <TARGET>"]
+parseCommand'' (CmdSpecTarget cs) (x:xs) = do
+  t <- findTarget x
+  fmap ($t) $ parseCommands'' cs xs
+parseCommand'' (CmdSpecConstant _ _) [] = lift $ Left []
+parseCommand'' (CmdSpecConstant c cs) (x:xs) | c == x = parseCommands'' cs xs
+                                             | otherwise = lift $ Left []
+parseCommand'' (CmdSpecNumber cs) [] = lift $ Left []
+parseCommand'' (CmdSpecNumber cs) (x:xs) = case readMaybe x of
+  Just n -> fmap ($n) $ parseCommands'' cs xs
+  Nothing -> lift $ Left ["Failed to parse number"]
+
+findTarget :: String -> ReaderT GameContext (Either [String]) (DescriptorIdent 'Truthy, [Thingy])
+findTarget _ = lift $ Left ["Target finding: To be implemented"]
+
+printHelp :: (MonadIO m) => CommandTree a -> m ()
+printHelp xs = void $ liftIO $ traverse putStrLn $ getHelp xs
+
+getHelp :: CommandTree a -> [String]
+getHelp = fmap (\(cmdParts, helpText) -> unwords cmdParts <> " - " <> helpText) . getHelp' "???"
+
+getHelp' :: String -> CommandTree a -> [([String], String)]
+getHelp' helpText tree = tree >>= getHelp'' helpText
+
+getHelp'' :: String -> CommandSpec a -> [([String], String)]
+getHelp'' helpText (CmdSpecConstant x xs) = fmap (first (x:)) (getHelp' helpText xs)
+getHelp'' helpText (CmdSpecLeaf _) = [([], helpText)]
+getHelp'' helpText (CmdSpecTarget xs) = fmap (first ("<TARGET>":)) (getHelp' helpText xs)
+getHelp'' _ (CmdSpecHelptext helpText x) = getHelp'' helpText x
+getHelp'' helpText (CmdSpecNumber xs) = fmap (first ("<NUMBER>":)) (getHelp' helpText xs)
 
 commandCompletion :: (Monad m) => GameDef -> CompletionFunc m
 commandCompletion _ = noCompletion
 
+type CommandTree a = [CommandSpec a]
 
--- commands = command "exit" CmdExit noArgs
---          <|> command "isReachable" CmdIsReachable truthyDescriptor
+data CommandSpec a =
+    CmdSpecConstant String (CommandTree a)
+  | CmdSpecLeaf a
+  | CmdSpecNumber (CommandTree (Integer -> a))
+  | CmdSpecTarget (CommandTree ((DescriptorIdent 'Truthy, [Thingy]) -> a))
+  | CmdSpecHelptext String (CommandSpec a)
+
+
+commands :: CommandTree Command
+commands = [ CmdSpecHelptext "exit the program" $ CmdSpecConstant "exit" $ pure $ CmdSpecLeaf CmdExit
+           , CmdSpecHelptext "Test whether the given target is reachable" $ CmdSpecConstant "isReachable" [CmdSpecTarget [CmdSpecLeaf CmdIsReachable]]
+           , CmdSpecConstant "step" $ [ CmdSpecHelptext "step the shuffler once" $ CmdSpecLeaf (CmdStep 1)
+                                      , CmdSpecHelptext "step the shuffler a number of times" $ CmdSpecNumber $ pure $ CmdSpecLeaf CmdStep
+                                      ]
+           , CmdSpecHelptext "step the shuffler until it is done" $ CmdSpecConstant "run" $ pure $ CmdSpecLeaf CmdRun
+           , CmdSpecHelptext "randomize the seed and reset shuffle progress" $ CmdSpecConstant "randomSeed" $ pure $ CmdSpecLeaf CmdRandomSeed
+           , CmdSpecHelptext "set the seed and reset shuffle progress" $ CmdSpecConstant "seed" $ pure $ CmdSpecNumber $ pure $ CmdSpecLeaf $ CmdSetSeed . mkStdGen . fromInteger
+           , CmdSpecHelptext "set the log level" $ CmdSpecConstant "logs" $ [ CmdSpecConstant "debug" $ pure $ CmdSpecLeaf $ CmdSetLogLevel LogLevelDebug
+                                                                            , CmdSpecConstant "error" $ pure $ CmdSpecLeaf $ CmdSetLogLevel LogLevelError
+                                                                            ]
+           ]
--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import Options.Applicative
-import TMCR.IO (readDirectoryFull, Directory (Directory), DirectoryErrorWithContext, readGameDefStrErr, runInMemoryDir)
+import TMCR.IO (readDirectoryFull, Directory (Directory), DirectoryErrorWithContext, readGameDefStrErr, runInMemoryDir, randomSeed)
 import TMCR.Logic.Merge (GameDef, defLogic)
 import System.Exit (exitFailure, exitSuccess)
 
@@ -18,17 +21,24 @@ import qualified Data.Map as M
 import qualified Polysemy.Error as P
 import qualified Polysemy.Reader as P
 import TMCR.Logic.Logic (Scopes (Scopes))
-import Control.Monad (when)
-import System.Console.Haskeline (InputT, defaultSettings, runInputT, outputStrLn, getInputLine, setComplete, haveTerminalUI)
-import Control.Monad.IO.Class (liftIO)
+import Control.Monad (when, replicateM)
+import System.Console.Haskeline (InputT, defaultSettings, runInputT, outputStrLn, getInputLine, setComplete, haveTerminalUI, mapInputT)
+import Control.Monad.IO.Class (liftIO, MonadIO)
 import TMCR.Logic.Descriptor (Descriptor, DescriptorIdent, Oolean, DescriptorType (Truthy, County))
 import TMCR.Logic.Common (Thingy, Nteger)
-import TMCR.Shuffler (ShuffleProgress, Consuming, LockIdent, Stateful, ShuffleDependent (LogicNodeDependent), initialShuffleProgress)
+import TMCR.Shuffler (ShuffleProgress, Consuming, LockIdent, Stateful, ShuffleDependent (LogicNodeDependent), initialShuffleProgress, updateLocal, getDefinitions, defaultEval, Definitions)
 import TMCR.Logic.Algebra (LogicValue, OolAble)
 import Control.Lens
+import Control.Lens.TH (makeLenses)
 import TMCR.Logic.Graphs (taggedGetNodes)
 import TMCR.Logic.Shuffle (RandomSeed)
 import Commands
+import Control.Monad.Reader (ReaderT(..))
+import Control.Monad.Trans ( MonadTrans(lift) )
+import System.Random (mkStdGen)
+import Control.Monad.State (StateT (runStateT), MonadState (..), gets)
+import Control.Monad.Except (ExceptT, runExceptT, MonadError (throwError))
+import Prelude hiding (log)
 
 
 data Options = Options {
@@ -40,18 +50,23 @@ data State = State {
     _stateGameDef :: GameDef
   , _stateOverridesTruthy :: Map (DescriptorIdent 'Truthy, [Thingy]) Oolean
   , _stateOverridesCounty :: Map (DescriptorIdent 'County, [Thingy]) (Nteger, Nteger)
-  , _stateProgress :: Maybe (ShuffleProgress (LogicValue (Consuming LockIdent (Stateful (OolAble Bool))) 'Truthy) (LogicValue (Consuming LockIdent (Stateful (OolAble Bool))) 'County))
+  , _stateProgress :: ShuffleProgress (LogicValue (Consuming LockIdent (Stateful (OolAble Bool))) 'Truthy) (LogicValue (Consuming LockIdent (Stateful (OolAble Bool))) 'County)
   , _stateWorklist :: [ShuffleDependent]
-  , _stateSeed :: Maybe RandomSeed
+  , _stateSeed :: RandomSeed
+  , _stateLoglevel :: LogLevel
 }
 
+$(makeLenses 'State)
 
-fromGameDef :: GameDef -> State
-fromGameDef gameDef = State gameDef M.empty M.empty Nothing (initialWorklist gameDef) Nothing where
-  initialWorklist :: GameDef -> [ShuffleDependent]
-  initialWorklist def = let nodes = def ^.. defLogic . _1 . to taggedGetNodes . folded . _Just
-                          --edges = foldMap (\n -> def ^.. defLogic . _1 . to (taggedGetEdgesTo (Just n)) . folded . _2 . to getDisjunctions . folded . folded) nodes
-                       in {-fmap DescriptorDependend edges <> -} fmap LogicNodeDependent nodes
+fromGameDef :: GameDef -> IO State
+fromGameDef gameDef = do
+  seed <- randomSeed
+  return $ State gameDef M.empty M.empty (initialShuffleProgress gameDef seed) (initialWorklist gameDef) seed LogLevelDebug
+
+initialWorklist :: GameDef -> [ShuffleDependent]
+initialWorklist def = let nodes = def ^.. defLogic . _1 . to taggedGetNodes . folded . _Just
+                        --edges = foldMap (\n -> def ^.. defLogic . _1 . to (taggedGetEdgesTo (Just n)) . folded . _2 . to getDisjunctions . folded . folded) nodes
+                      in {-fmap DescriptorDependend edges <> -} fmap LogicNodeDependent nodes
 
 
 options :: Parser Options
@@ -75,7 +90,7 @@ runApp options = do
       success <- runInputT (setComplete (commandCompletion gameDef) defaultSettings) $ do
         interactive <- if (non_interactive options) then return False else haveTerminalUI
         if interactive then
-          app options (fromGameDef gameDef)
+          liftIO (fromGameDef gameDef) >>= app options 
         else return True
       if success then exitSuccess else exitFailure
 
@@ -87,11 +102,72 @@ app options s = do
     Left e -> return e
 
 repl :: Options -> State -> InputT IO (Either Bool State)
-repl options state = do
-  cmd <- parseCommand
-  case cmd of
-    CmdExit -> return $ Left True
-    CmdHelp topic -> outputStrLn (commandHelp topic) >> return (Right state)
+repl _options state = do
+  cmd <- runReaderT parseCommand GameContext
+  x <- runStateT (runExceptT $ runCommand cmd) state
+  case x of
+    (Left exit, _) -> return $ Left exit
+    (Right _, state') -> return $ Right state'
+
+runCommand :: (MonadIO m) => Command -> ExceptT Bool (StateT State m) ()
+runCommand cmd = case cmd of
+    CmdExit -> throwError True
+    CmdStep n -> do
+      debug $ "Step " <> show n
+      step n
+    CmdRun -> do
+      debug "Run"
+      runCommand $ CmdStep 10000
+    CmdIsReachable target -> do
+      debug "isReachable"
+    CmdRandomSeed -> do
+      debug "Random Seed"
+      seed <- liftIO randomSeed
+      runCommand $ CmdSetSeed seed
+    CmdSetSeed seed -> do
+      debug "Set Seed"
+      gameDef <- gets $ view stateGameDef
+      stateSeed .= seed
+      stateProgress .= initialShuffleProgress gameDef seed
+      stateWorklist .= initialWorklist gameDef
+    CmdSetLogLevel level -> stateLoglevel .= level
+
+log :: (MonadIO m) => LogLevel -> String -> ExceptT Bool (StateT State m) ()
+log level msg = gets (view stateLoglevel) >>= \level' -> if level' <= level then liftIO $ putStrLn msg else return ()
+
+debug :: (MonadIO m) => String -> ExceptT Bool (StateT State m) ()
+debug = log LogLevelDebug
+
+step :: (MonadIO m) => Integer -> ExceptT Bool (StateT State m) ()
+step 0 = return ()
+step n | n < 0 = return ()
+       | otherwise = do
+        first <- popWorklist
+        case first of
+          Nothing -> return ()
+          Just target -> do
+            debug $ "Stepping on: " <> show target
+            updateOn target
+            step (n - 1)
+        return ()
+
+updateOn :: (Monad m) => ShuffleDependent -> ExceptT Bool (StateT State m) ()
+updateOn target = do
+  defs <- gets $ view stateDefinitions
+  progress <- gets $ view stateProgress
+  let (newShuffleProgress, addToWorklist) = updateLocal defs defaultEval target progress
+  stateProgress .= newShuffleProgress
+  stateWorklist <>= addToWorklist
+
+--todo: add overrides
+stateDefinitions :: Getter State Definitions
+stateDefinitions = stateGameDef . to getDefinitions
+
+popWorklist :: (Monad m) => ExceptT Bool (StateT State m) (Maybe ShuffleDependent)
+popWorklist = zoom stateWorklist $ state $ \case
+  [] -> (Nothing, [])
+  (x:xs) -> (Just x, filter (/= x) xs)
+
 
 loadGameDef options = compile <$> readDirectoryFull (module_directory options)
 
